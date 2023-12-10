afbdeaa4f165aa3bb7618d93d26de687ed38b28c
Chamod Udara <128582580+uchamod@users.noreply.github.com>
2023-12-10 20:20:09 +0530

    Initial commit

---
+++ src/texteditor/Texteditor.java
@@ -0,0 +1,12 @@
+package texteditor;
+
+public class Texteditor {
+
+    public static void main(String[] args) {
+        TextInterface TI = new TextInterface();
+        TI.setVisible(true);
+        TI.setTitle("Basic Text Editor");
+        TI.setLocationRelativeTo(null);
+    }
+
+}
